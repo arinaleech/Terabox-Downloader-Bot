999404c009286a543c986c478d39e8c679965657
arinaleech <ndjdn6062@gmail.com>
2025-01-17 11:29:11 +0530

    Update video.py

--- video.py
+++ video.py
@@ -86,57 +86,57 @@ async def download_video(url, reply_msg, user_mention, user_id):
         )
         return None, None, None
 
-# async def download_video(url, reply_msg, user_mention, user_id):
-#     response = requests.get(f"https://teraboxvideodownloader.nepcoderdevs.workers.dev/?url={url}")
-#     response.raise_for_status()
-#     data = response.json()
+ async def download_video(url, reply_msg, user_mention, user_id):
+     response = requests.get(f"https://teraboxvideodownloader.nepcoderdevs.workers.dev/?url={url}")
+     response.raise_for_status()
+    data = response.json()
 
-#     resolutions = data["response"][0]["resolutions"]
-#     fast_download_link = resolutions["Fast Download"]
-#     hd_download_link = resolutions["HD Video"]
-#     thumbnail_url = data["response"][0]["thumbnail"]
-#     video_title = data["response"][0]["title"]
+     resolutions = data["response"][0]["resolutions"]
+     fast_download_link = resolutions["Fast Download"]
+     hd_download_link = resolutions["HD Video"]
+     thumbnail_url = data["response"][0]["thumbnail"]
+     video_title = data["response"][0]["title"]
 
-#     download = aria2.add_uris([fast_download_link])
-#     start_time = datetime.now()
+     download = aria2.add_uris([fast_download_link])
+     start_time = datetime.now()
 
-#     while not download.is_complete:
-#         download.update()
-#         percentage = download.progress
-#         done = download.completed_length
-#         total_size = download.total_length
-#         speed = download.download_speed
-#         eta = download.eta
-#         elapsed_time_seconds = (datetime.now() - start_time).total_seconds()
-#         progress_text = format_progress_bar(
-#             filename=video_title,
-#             percentage=percentage,
-#             done=done,
-#             total_size=total_size,
-#             status="Downloading",
-#             eta=eta,
-#             speed=speed,
-#             elapsed=elapsed_time_seconds,
-#             user_mention=user_mention,
-#             user_id=user_id,
-#             aria2p_gid=download.gid
-#         )
-#         await reply_msg.edit_text(progress_text)
-#         await asyncio.sleep(2)
+     while not download.is_complete:
+         download.update()
+         percentage = download.progress
+         done = download.completed_length
+         total_size = download.total_length
+         speed = download.download_speed
+         eta = download.eta
+         elapsed_time_seconds = (datetime.now() - start_time).total_seconds()
+         progress_text = format_progress_bar(
+             filename=video_title,
+             percentage=percentage,
+             done=done,
+             total_size=total_size,
+            status="Downloading",
+             eta=eta,
+             speed=speed,
+             elapsed=elapsed_time_seconds,
+             user_mention=user_mention,
+            user_id=user_id,
+             aria2p_gid=download.gid
+         )
+         await reply_msg.edit_text(progress_text)
+         await asyncio.sleep(2)
 
-#     if download.is_complete:
-#         file_path = download.files[0].path
+     if download.is_complete:
+         file_path = download.files[0].path
 
-#         thumbnail_path = "thumbnail.jpg"
-#         thumbnail_response = requests.get(thumbnail_url)
-#         with open(thumbnail_path, "wb") as thumb_file:
-#             thumb_file.write(thumbnail_response.content)
+         thumbnail_path = "thumbnail.jpg"
+         thumbnail_response = requests.get(thumbnail_url)
+         with open(thumbnail_path, "wb") as thumb_file:
+             thumb_file.write(thumbnail_response.content)
 
-#         await reply_msg.edit_text("ᴜᴘʟᴏᴀᴅɪɴɢ...")
+         await reply_msg.edit_text("ᴜᴘʟᴏᴀᴅɪɴɢ...")
 
-#         return file_path, thumbnail_path, video_title
-#     else:
-#         return markup
+         return file_path, thumbnail_path, video_title
+     else:
+         return markup
 
 
 async def upload_video(client, file_path, thumbnail_path, video_title, reply_msg, collection_channel_id, user_mention, user_id, message):
